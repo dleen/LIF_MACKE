49abe9b44f757b2f382612359b54950fd4a4c046
David Leen <dleen@uw.edu>
2012-01-08 16:33:59 -0800

    merged main

--- main.cpp
+++ main.cpp
@@ -8,18 +8,10 @@ using namespace std;
 int main()
 {
 
-//	create_figure_1a();
-<<<<<<< HEAD
-
-	LIF_spike Y(5);
-	Y.create_LIF_data(0.18,0.7);
-	Y.print_statistics();
-=======
-	create_figure_1b();
+//	create_figure_1b();
 
 //	LIF_spike Y(5);
 //	Y.create_LIF_data(-2.25,0.7);
 //	Y.print_statistics();
->>>>>>> origin/algorithm-update
 
 }
